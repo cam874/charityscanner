fa86ebdec378c5a4b85ba746150c4fe058fdede2
cam874 <144983790+cam874@users.noreply.github.com>
2025-06-18 13:09:10 +0000

    Add debugging code and CORS support to fix frontend issues

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use((req, res, next) => {
     next();
 });
 
+// Add CORS headers for development
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 // Serve the static frontend file from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
